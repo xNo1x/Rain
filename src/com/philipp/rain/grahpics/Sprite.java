44fc65b5cbc4ba242f6a5165f624681bd9841587
xNo1x <philippkillian@gmail.com>
2014-08-29 18:44:47 +0200

    After ep 51

--- src/com/philipp/rain/grahpics/Sprite.java
+++ src/com/philipp/rain/grahpics/Sprite.java
@@ -8,6 +8,8 @@ public class Sprite {
 	private SpriteSheet sheet;
 
 	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);
+	public static Sprite flower = new Sprite(16, 1, 0, SpriteSheet.tiles);
+	public static Sprite rock = new Sprite(16, 2, 0, SpriteSheet.tiles);
 	public static Sprite voidSprite = new Sprite(16, 0x1B87E0);
 
 	public static Sprite player_forward = new Sprite(32, 0, 5, SpriteSheet.tiles);
