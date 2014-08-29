e95b2fb7e669c48d35a14ff2c8b4682b26ac0e01
xNo1x <philippkillian@gmail.com>
2014-08-29 18:12:44 +0200

    first try

---
+++ src/com/philipp/rain/entity/Entity.java
@@ -0,0 +1,30 @@
+package com.philipp.rain.entity;
+
+import java.util.Random;
+
+import com.philipp.rain.grahpics.Screen;
+import com.philipp.rain.level.Level;
+
+public abstract class Entity {
+
+	public int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+
+	public void update() {
+
+	}
+
+	public void render(Screen screen) {
+
+	}
+
+	public void remove() {
+		removed = true;
+	}
+
+	public boolean isRemoved() {
+		return removed;
+	}
+}
---
+++ src/com/philipp/rain/entity/mob/Mob.java
@@ -0,0 +1,33 @@
+package com.philipp.rain.entity.mob;
+
+import com.philipp.rain.entity.Entity;
+import com.philipp.rain.grahpics.Sprite;
+
+public abstract class Mob extends Entity {
+
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+
+	public void move(int xa, int ya) {
+		if (xa > 0) dir = 1;
+		if (xa < 0) dir = 3;
+		if (ya > 0) dir = 2;
+		if (ya < 0) dir = 0;
+
+		if (!collision()) {
+			x += xa;
+			y += ya;
+		}
+	}
+
+	public void update() {
+	}
+
+	private boolean collision() {
+		return false;
+	}
+
+	public void render() {
+	}
+}
