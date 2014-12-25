ec05572345b4176260097e617163ceb0e4b5e023
xNo1x <philippkillian@gmail.com>
2014-12-25 20:55:07 +0100

    episode 56

--- src/com/philipp/rain/level/Level.java
+++ src/com/philipp/rain/level/Level.java
@@ -5,24 +5,26 @@ import com.philipp.rain.level.tile.Tile;
 
 public class Level {
 
+	protected Tile[] tiles;
 	protected int width, height;
-	protected int[] tiles;
+	protected int[] tilesInt;
 
 	public Level(int width, int height) {
 		this.width = width;
 		this.height = height;
-		tiles = new int[width * height];
+		tilesInt = new int[width * height];
 		generateLevel();
 	}
 
 	public Level(String path) {
 		loadLevel(path);
+		generateLevel();
 	}
 
 	protected void generateLevel() {
 	}
 
-	private void loadLevel(String path) {
+	protected void loadLevel(String path) {
 	}
 
 	public void update() {
@@ -41,16 +43,21 @@ public class Level {
 		for (int y = y0; y < y1; y++) {
 			for (int x = x0; x < x1; x++) {
 				getTile(x, y).render(x, y, screen);
+
 			}
 		}
 
 	}
 
 	public Tile getTile(int x, int y) {
-		if (x < 0 || y < 0 || x > width || y > height) return Tile.voidTile;
-		if (tiles[x + y * width] == 0) return Tile.grass;
-		if (tiles[x + y * width] == 1) return Tile.flower;
-		if (tiles[x + y * width] == 2) return Tile.rock;
+		if (x < 0 || y < 0 || x >= width || y >= height)
+			return Tile.voidTile;
+		if (tilesInt[x + y * width] == 0)
+			return Tile.grass;
+		if (tilesInt[x + y * width] == 1)
+			return Tile.flower;
+		if (tilesInt[x + y * width] == 2)
+			return Tile.rock;
 		return Tile.voidTile;
 
 	}
